078d43c994c1f35d72f3b75155fdbbf60a89aac7
Masatoshi HANAI <hanahana0201@gmail.com>
2019-06-04 07:39:23 +0900

    creat keymap for miniaxe

--- keyboards/miniaxe/keymaps/hanachan/keymap.c
+++ keyboards/miniaxe/keymaps/hanachan/keymap.c
@@ -44,9 +44,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_QWERTY] = LAYOUT( \
   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    \
-  LSFT_T(KC_A),KC_S,KC_D,    KC_F,    KC_G,         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, \
-  KC_Z,LALT_T(KC_X),KC_C,    KC_V,    KC_B,         KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, \
-                    LCMD_T(KC_LANG2),LT(_LOWER, KC_SPC),LCTL_T(KC_BSPC),   RCTL_T(KC_BSPC),LT(_RAISE, KC_ENT),RCMD_T(KC_LANG1)  \
+  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, \
+  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,         KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, \
+                    KC_LGUI, LOWER, MT(MOD_LCTL, KC_ESC),   MT(MOD_LSFT, KC_SPC), RAISE, MT(MOD_LALT, KC_BSPC)  \
 ),
 
 /* Raise
@@ -75,14 +75,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------|           |------+------+------+------+------|
  * |  Tab |      |      |      |      |           | Left | Down |  Up  | Right| Enter|
  * |------+------+------+------+------|           |------+------+------+------+------|
- * |  Ctrl|   `  |  GUI |  Alt | Del  |           | BkSp | PgUp | PgDn |   \  |   '  |
+ * | Shift|  Alt |  GUI |  Ctrl| Del  |           | BkSp | PgUp | PgDn |   \  |   '  |
  * `-------------+------+------+------|           |------+------+------+------+------'
  *               |      | LOWER|      |           |      | RAISE|      |
  *               `--------------------'           `--------------------'
  */
 [_LOWER] = LAYOUT( \
   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    \
-  KC_TAB,  _______, _______, _______, _______,     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_ESC, \
+  KC_TAB,  _______, _______, _______, _______,     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_ENT, \
   KC_LSFT, KC_LALT, KC_LGUI, KC_LCTL, KC_DEL,      KC_BSPC, KC_PGUP, KC_PGDN, KC_BSLS, KC_QUOT, \
                     _______, _______, _______,      _______, _______, _______                    \
 ),
