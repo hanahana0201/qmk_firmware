36f997ac28d3a407867792a6a3f70150b18eae0d
Masatoshi HANAI <hanahana0201@gmail.com>
2019-06-06 12:25:41 +0900

    creat keymap for miniaxe

--- keyboards/miniaxe/keymaps/hanachan/keymap.c
+++ keyboards/miniaxe/keymaps/hanachan/keymap.c
@@ -48,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    \
   KC_A,    KC_S,    KC_D,    KC_F,    KC_G,         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, \
   LT(_NUM, KC_Z),    KC_X,    KC_C,    KC_V,    KC_B,         KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, \
-                    KC_LGUI, LOWER, MT(MOD_LCTL, KC_ESC),   MT(MOD_LSFT, KC_SPC), RAISE, MT(MOD_LALT, KC_BSPC)  \
+                    MT(MOD_LGUI, KC_SPC), LOWER, MT(MOD_LCTL, KC_ESC),   MT(MOD_LSFT, KC_SPC), RAISE, MT(MOD_LALT, KC_BSPC)  \
 ),
 
 /* Raise
@@ -73,7 +73,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Lower
  *
  * ,----------------------------------.           ,----------------------------------.
- * |   1  |   2  |   3  |   4  |   5  |           |   6  |   7  |   8  |   9  |   0  |
+ * |      |      |      |      |      |           |      |      |      |      |      |
  * |------+------+------+------+------|           |------+------+------+------+------|
  * |  Tab |      |      |      |      |           | Left | Down |  Up  | Right| Enter|
  * |------+------+------+------+------|           |------+------+------+------+------|
@@ -83,7 +83,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *               `--------------------'           `--------------------'
  */
 [_LOWER] = LAYOUT( \
-  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    \
+  _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______,    \
   KC_TAB,  _______, _______, _______, _______,     KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_ENT, \
   KC_LSFT, KC_LALT, KC_LGUI, KC_LCTL, KC_DEL,      KC_BSPC, KC_PGUP, KC_PGDN, KC_BSLS, KC_QUOT, \
                     _______, _______, _______,      _______, _______, _______                    \
@@ -92,11 +92,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* NUM
  *
  * ,----------------------------------.           ,----------------------------------.
- * |      |   0  |   1  |   2  |   3  |           |      |      |      |      |      |
+ * |   0  |   1  |   2  |   3  |      |           |      |      |      |      |      |
  * |------+------+------+------+------|           |------+------+------+------+------|
- * |  Tab |   -  |   4  |   5  |   6  |           |      |      |      |      |      |
+ * |  Tab |   4  |   5  |   6  |   -  |           |      |      |      |      |      |
  * |------+------+------+------+------|           |------+------+------+------+------|
- * |      |   +  |   7  |   8  |   9  |           |      |      |      |      |   '  |
+ * |      |   7  |   8  |   9  |   +  |           |      |      |      |      |   '  |
  * `-------------+------+------+------|           |------+------+------+------+------'
  *               |   *  |   .  |   /  |           |      |             |
  *               `--------------------'           `--------------------'
